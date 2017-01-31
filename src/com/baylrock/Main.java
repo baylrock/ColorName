dc8ecb0d6ee195b0be74a95e24ec1f7596f86e67
Baylrock <skidi@i.ua>
2017-01-31 14:13:53 +0200

    cleanup

--- src/com/baylrock/Main.java
+++ src/com/baylrock/Main.java
@@ -1,14 +1,8 @@
 package com.baylrock;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.File;
-import java.io.FileOutputStream;
-
 public class Main {
 
     public static void main( String[] args ) {
-       Colors.doCacheFullMap();
+       Colors.doCacheFullMap(10);
     }
 }
