eb440b06a464ac8df955033695ffe25691b2bb8a
Baylrock <skidi@i.ua>
2017-01-31 13:58:19 +0200

    init

---
+++ src/com/baylrock/Main.java
@@ -0,0 +1,14 @@
+package com.baylrock;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.File;
+import java.io.FileOutputStream;
+
+public class Main {
+
+    public static void main( String[] args ) {
+       Colors.doCacheFullMap();
+    }
+}
